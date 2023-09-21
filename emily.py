0cba3992bcddf0b261ca560da011cf0327ec3d39
AmirHossein <email@ahshams.top>
2023-09-22 02:27:46 +0330

    writing mode added

--- emily.py
+++ emily.py
@@ -35,43 +35,46 @@ def get_file_url(message , content_type):
 
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
+    bot.send_chat_action(message.chat.id, 'typing')
     bot.reply_to(message, describe_image(get_file_url(message , 'photo')))
 
 
 @bot.message_handler(content_types=['sticker'])
-def describe_photo(message):
+def handle_sticker(message):
+    bot.send_chat_action(message.chat.id, 'typing')
     bot.reply_to(message, describe_image(get_file_url(message , 'sticker')))
     
 @bot.message_handler(content_types=['animation'])
-def describe_photo(message):
-     try:
-        # Create a temporary directory
-        with tempfile.TemporaryDirectory() as temp_dir:
-            mp4_file_path = os.path.join(temp_dir, "video.mp4")
+def handle_animation(message):
+        bot.send_chat_action(message.chat.id, 'typing')
+        try:
+            # Create a temporary directory
+            with tempfile.TemporaryDirectory() as temp_dir:
+                mp4_file_path = os.path.join(temp_dir, "video.mp4")
 
-            # Download the MP4 file
-            response = requests.get(get_file_url(message , 'animation'))
-            if response.status_code == 200:
-                with open(mp4_file_path, 'wb') as f:
-                    f.write(response.content)
-            else:
-                return None
+                # Download the MP4 file
+                response = requests.get(get_file_url(message , 'animation'))
+                if response.status_code == 200:
+                    with open(mp4_file_path, 'wb') as f:
+                        f.write(response.content)
+                else:
+                    return None
 
-            # Extract the middle frame
-            cap = cv2.VideoCapture(mp4_file_path)
-            frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-            middle_frame_idx = frame_count // 2
-            success, frame = cap.read()
-            if success:
-                middle_frame_path = os.path.join(temp_dir, "middle_frame.jpg")
-                cv2.imwrite(middle_frame_path, frame)
-                bot.reply_to(message, describe_image(str(middle_frame_path)))
-            else:
-                return None
+                # Extract the middle frame
+                cap = cv2.VideoCapture(mp4_file_path)
+                frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+                middle_frame_idx = frame_count // 2
+                success, frame = cap.read()
+                if success:
+                    middle_frame_path = os.path.join(temp_dir, "middle_frame.jpg")
+                    cv2.imwrite(middle_frame_path, frame)
+                    bot.reply_to(message, describe_image(str(middle_frame_path)))
+                else:
+                    return None
 
-     except Exception as e:
-         print(f"{e}")
-         return None
+        except Exception as e:
+            print(f"{e}")
+            return None
     
 
 bot.infinity_polling()
